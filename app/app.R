f609a991883247f88a666c0119409950678c3298
Derek Chen <derekjchen@proton.me>
2026-03-15 12:34:19 -0400

    finalised app logic (excluding participant information)

--- app/app.R
+++ app/app.R
@@ -1,48 +1,152 @@
 library(shiny)
 library(bslib)
+library(readr)
+library(shinyjs)
 
-# Define UI for app that draws a histogram ----
-ui <- page_sidebar(
-  # App title ----
-  title = "Hello Shiny!",
-  # Sidebar panel for inputs ----
-  sidebar = sidebar(
-    # Input: Slider for the number of bins ----
-    sliderInput(
-      inputId = "bins",
-      label = "Number of bins:",
-      min = 1,
-      max = 50,
-      value = 30
-    )
+# Read questions
+questions <- read_csv("questions.csv")
+
+ui <- page_fillable(
+  # Styles and app behaviour
+  theme = bs_theme(version = 5, bootswatch = "flatly"),
+  tags$head(
+    useShinyjs(),
+    includeCSS("www/style.css"),
+    includeScript("www/script.js")
   ),
-  # Output: Histogram ----
-  plotOutput(outputId = "distPlot")
+
+  # Main content
+  div(
+    class = "main-content",
+    div(class = "timer-wrapper", div(id = "timer")),
+    card(class = "main-card", uiOutput("ui_content"))
+  )
 )
 
-# Define server logic required to draw a histogram ----
-server <- function(input, output) {
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
-  output$distPlot <- renderPlot({
-    x <- faithful$waiting
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+server <- function(input, output, session) {
+  # Randomly sample for presence of timer feedback
+  feedback <- sample(c("timer", "no_timer"), 1)
+  print(paste("Feedback:", feedback))
 
-    hist(
-      x,
-      breaks = bins,
-      col = "#007bc2",
-      border = "white",
-      xlab = "Waiting time to next eruption (in mins)",
-      main = "Histogram of waiting times"
+  rv <- reactiveValues(
+    state = "instructions",
+    qnum = 1,
+    responses = data.frame(),
+    question_start = NULL
+  )
+
+  # Ending test
+  end_test <- function() {
+    rv$state <- "done"
+
+    # Stop timer
+    runjs("endTestPhase();")
+
+    # Store detailed results in dedicated csv
+    if (!dir.exists("results")) {
+      dir.create("results")
+    }
+    write_csv(
+      rv$responses,
+      paste0(
+        "results/",
+        format(Sys.time(), "%Y-%m-%d_%H-%M-%S_"),
+        feedback,
+        ".csv"
+      )
     )
+  }
+
+  # Main UI pages (instructions -> test -> done)
+  output$ui_content <- renderUI({
+    if (rv$state == "instructions") {
+      tagList(
+        h1("Mental Arithmetic Test"),
+        div(
+          p(
+            "You will be presented with 50 mental addition (+), subtraction (-), multiplication (*), and division (/) problems of various difficulties."
+          ),
+          p("To the best of your ability, answer as many questions correctly."),
+          class = "instructions"
+        ),
+        actionButton("start", "Begin")
+      )
+    } else if (rv$state == "test") {
+      tagList(
+        div(paste("Question", rv$qnum, "of 50"), class = "progress-text"),
+        div(questions$question[rv$qnum], class = "question-text"),
+        div(
+          textInput("user_ans", label = NULL),
+          class = "centered-input-wrap"
+        ),
+      )
+    } else {
+      tagList(
+        h1("Thank you for participating!"),
+        p(
+          "The test has ended. Your results have been recorded. Have a good day."
+        )
+      )
+    }
+  })
+
+  # On test start
+  observeEvent(input$start, {
+    rv$state <- "test"
+
+    # Record question start time
+    rv$question_start <- Sys.time()
+
+    # Show timer depending on feedback condition
+    if (feedback == "timer") {
+      runjs("$('#timer').show();")
+    } else {
+      runjs("$('#timer').hide();")
+    }
+
+    # Start the test phase behaviour
+    runjs("startTestPhase();")
+  })
+
+  # On submission of an answer
+  observeEvent(input$submit_val, {
+    req(rv$state == "test")
+
+    # Store question data
+    question <- questions$question[rv$qnum]
+    answer = questions$answer[rv$qnum]
+    submission = input$submit_val
+    response_time <- as.numeric(difftime(
+      Sys.time(),
+      rv$question_start,
+      units = "secs"
+    ))
+    rv$responses <- rbind(
+      rv$responses,
+      data.frame(
+        qnum = rv$qnum,
+        question = question,
+        answer = answer,
+        submission = submission,
+        correct = submission == answer,
+        response_time = response_time
+      )
+    )
+
+    # Move on to next question unless at the last question
+    if (rv$qnum < nrow(questions)) {
+      rv$qnum <- rv$qnum + 1
+      rv$question_start <- Sys.time()
+    } else {
+      end_test()
+    }
+  })
+
+  # Finish test if time is up
+  observeEvent(input$time_up, {
+    req(rv$state == "test")
+    end_test()
   })
 }
 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
